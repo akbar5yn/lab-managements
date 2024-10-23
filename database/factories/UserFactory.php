375c5089c5e55e865c765bf33dea815507c69bc7
Akbar <akbar5yn.developer@gmail.com>
2024-10-24 03:15:42 +0700

    create factory user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,12 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    protected static ?string $password;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +20,23 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'nim' => $this->faker->unique()->randomNumber(8), // Menghasilkan NIM acak yang unik
+            'username' => $this->faker->unique()->userName(), // Menghasilkan username yang unik
+            'password' => Hash::make('password'), // Menghasilkan password yang telah di-hash
+            'role' => $this->faker->randomElement(['laboran', 'mahasiswa']), // Menghasilkan peran acak
         ];
+    }
+
+    /**
+     * State untuk menghasilkan mahasiswa.
+     *
+     * @return static
+     */
+    public function mahasiswa(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'mahasiswa', // Atur role menjadi mahasiswa
+        ]);
     }
 }
