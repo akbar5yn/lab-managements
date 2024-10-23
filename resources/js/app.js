bc93fbc0db2be0d94a046f304c38cb138da63370
Akbar <akbar5yn.developer@gmail.com>
2024-10-24 03:10:13 +0700

    adding notification sweet alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import './bootstrap';
+import Swal from 'sweetalert2';
+
+function showAlert(title, text, icon) {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        confirmButtonText: 'OK',
+    });
+}
+
+window.showAlert = showAlert;
