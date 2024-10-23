01aa03191f0d548fcf21b4156ebe915ddeb7305e
Akbar <akbar5yn.developer@gmail.com>
2024-10-24 03:17:16 +0700

    call user seeder for using

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            UserSeeder::class,
+
+            // Tambahkan seeder lainnya jika ada
+        ]);
     }
 }
