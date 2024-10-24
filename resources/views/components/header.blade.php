e1f5cce1deac741f24203b400ecec81157978a12
Akbar <akbar5yn.developer@gmail.com>
2024-10-24 23:23:57 +0700

    create header display code

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<header class="header flex h-[60px] items-center justify-between rounded-xl bg-[#FFFFFF] px-4 py-5 shadow-md">
+    <div class="flex items-center gap-2">
+        @if (isset($subtitle) && isset($subtitle))
+            <h1 class="text-xl font-medium">{{ $title }}</h1>
+            <x-heroicon-m-chevron-right class="w-5" />
+            <h2 class="text-xl font-normal">{{ $subtitle }}</h2>
+        @else
+            <h1 class="text-xl font-medium">{{ $title }}</h1>
+        @endif
+    </div>
+    <h4>Hi, Akbar Pratama S</h4>
+</header>
