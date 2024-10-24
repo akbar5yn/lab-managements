b6be7d57e0340c252cefe75ae362c47cb1e4bda8
Akbar <akbar5yn.developer@gmail.com>
2024-10-25 05:06:36 +0700

    create controller for get data Unit

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\InventarisAlat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UnitController extends Controller
 {
-    //
+    public function getUnitAlat($slug)
+    {
+        $user = Auth::user();
+        $title = 'Kelola Alat & Barang';
+        $subtitle = 'Detail Alat';
+        $name = $user->name;
+        $role = $user->role;
+        $alat = InventarisAlat::with('alat')->where('slug', $slug)->firstOrFail();
+
+        $units = $alat->alat;
+
+        //ANCHOR - Informasi Unit
+        $countTersedia = $alat->alat()->where('status', 'Tersedia')->count();
+        $countDipinjam = $alat->alat()->where('status', 'Dipinjam')->count();
+        $countRusak = $alat->alat()->where('kondisi', 'Rusak')->count();
+        $countNormal = $alat->alat()->where('kondisi', 'Normal')->count();
+        $countTotal = $alat->alat()->count();
+
+        return view(
+            'laboran.unit',
+            compact(
+                'title',
+                'subtitle',
+                'name',
+                'role',
+                'alat',
+                'units',
+                'countTersedia',
+                'countDipinjam',
+                'countRusak',
+                'countNormal',
+                'countTotal'
+            )
+            // [
+            //     'units' => $units,
+            //     'countTersedia' => $countTersedia,
+            //     'countDipinjam' => $countDipinjam,
+            //     'countRusak' => $countRusak,
+            //     'countNormal' => $countNormal,
+            //     'countTotal' => $countTotal,
+            // ]
+        );
+    }
 }
