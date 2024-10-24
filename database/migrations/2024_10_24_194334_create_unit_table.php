54c71f90f4e7e757b59eb5ae893fc991e6a4e733
Akbar <akbar5yn.developer@gmail.com>
2024-10-25 02:44:58 +0700

    add unit migration

---
+++ database/migrations/2024_10_24_194334_create_unit_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unit', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_unit')->unique();
+            $table->enum('status', ['Tersedia', 'Dipinjam', 'Rusak'])->default('Tersedia');
+            $table->enum('kondisi', ['Normal', 'Rusak'])->default('Normal');
+            $table->foreignId('equipment_category_id')->constrained(
+                table: 'equipment_categories',
+                indexName: 'equipment_categories_id'
+            );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unit');
+    }
+};
