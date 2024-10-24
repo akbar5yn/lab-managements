487b8fdc41f60e6e794685a95dbb28304d3d114e
Akbar <akbar5yn.developer@gmail.com>
2024-10-25 05:02:01 +0700

    create route to update, delete, post and, get

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\InventarisAlatController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\UnitController;
 use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +14,11 @@ Route::post('/', [LoginController::class, 'authenticate'])->name('authenticate')
 Route::get('/laboran/dashboard', [DashboardController::class, 'indexLaboran'])->middleware(CheckRole::class . ':laboran')->name('laboran');
 
 Route::get('/mahasiswa/dashboard', [DashboardController::class, 'indexMahasiswa'])->middleware(CheckRole::class . ':mahasiswa')->name('mahasiswa');
+
+//SECTION - Route Inventory Alat
+Route::get('/laboran/inventaris-alat', [InventarisAlatController::class, 'index'])->name('inventaris-alat');
+Route::post('/laboran/inventaris-alat', [InventarisAlatController::class, 'handleRequest'])->name('post.alat');
+Route::put('/laboran/inventaris-alat/{id}', [InventarisAlatController::class, 'handleUpdate'])->name('edit.alat');
+Route::delete('/laboran/inventaris-alat{id}', [InventarisAlatController::class, 'handleDelete'])->name('delete.alat');
+
+Route::get('/laboran/inventaris-alat/{slug}', [UnitController::class, 'getUnitAlat'])->name('alat.unit');
