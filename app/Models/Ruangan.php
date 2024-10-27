d222367d085b8d610eb9af646fd95155cbb137e2
Akbar <akbar5yn.developer@gmail.com>
2024-10-27 21:55:08 +0700

    create protected table and fillable on model ruangan

--- app/Models/Ruangan.php
+++ app/Models/Ruangan.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
 
 class Ruangan extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'ruangan';
+    protected $fillable = ['nama_ruangan', 'lokasi_ruangan', 'kapasitas'];
 }
