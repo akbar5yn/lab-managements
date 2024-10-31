f991f3faae3517e0b722008f90222a1de01c6e2d
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 03:03:21 +0700

    create view pengajuan peminjaman alat

---
+++ resources/views/laboran/pengajuan-peminjaman-alat.blade.php
@@ -0,0 +1,36 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:name>{{ $name }}</x-slot:name>
+    <x-slot:role>{{ $role }}</x-slot:role>
+    <main class="flex h-full flex-col gap-4">
+        <!-- SECTION Button pengajuan / peminjaman-->
+        <section class="flex max-w-full gap-4 rounded-xl bg-white p-4 shadow-md">
+            <a href="{{ route('peminjaman.alat.pengajuan') }}"
+                class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
+            <a href="{{ route('peminjaman.alat.berlangsung') }}"
+                class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Peminjaman</a>
+        </section>
+
+        <section class="h-full overflow-y-scroll rounded-xl bg-white shadow-md">
+            <div class="p-4">
+                <div
+                    class="sticky top-0 z-10 grid grid-cols-[4%_20%_25%_25%_15%_auto] items-center border-b border-gray-400 bg-[#e4e4e4] shadow">
+                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                        No</p>
+                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                        Tanggal Kembali</p>
+                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                        Nama Peminjam</p>
+                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                        Barang Dipinjam</p>
+                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                        Status</p>
+                    <p class="flex h-full items-center justify-center px-2 py-2 text-center">
+                        Aksi</p>
+                </div>
+
+
+            </div>
+        </section>
+    </main>
+</x-layout>
