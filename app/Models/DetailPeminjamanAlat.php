1f0b06176fa6c86ebb9bced4a23b757e5c88cb59
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 01:50:23 +0700

    create model and relation for detail-peminjaman-alat

---
+++ app/Models/DetailPeminjamanAlat.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class DetailPeminjamanAlat extends Model
+{
+    protected $table = 'detail_peminjaman_alat';
+    protected $fillable = ['id_transaksi_peminjaman', 'id_alat', 'jumlah'];
+
+    public function relasiTransaksiPeminjamanAlat(): BelongsTo
+    {
+        return $this->belongsTo(TransaksiPeminjamanAlat::class, 'id_transaksi_peminjaman');
+    }
+
+    public function relasiUnitBarang(): BelongsTo
+    {
+        return $this->belongsTo(Unit::class, 'id_alat');
+    }
+}
