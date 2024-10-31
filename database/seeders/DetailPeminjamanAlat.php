d21145e454dd10e992b1506985435047a543ef3e
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 03:00:44 +0700

    create data seeder for detail peminjaman alat

--- database/seeders/DetailPeminjamanAlat.php
+++ database/seeders/DetailPeminjamanAlat.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DetailPeminjamanAlat extends Seeder
 {
@@ -12,6 +14,44 @@ class DetailPeminjamanAlat extends Seeder
      */
     public function run(): void
     {
-        //
+        $detail_alat = [
+            [
+                'id_transaksi_peminjaman' => 1,
+                'id_alat' => 1,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 10),
+            ],
+            [
+                'id_transaksi_peminjaman' => 1, // Masih transaksi yang sama
+                'id_alat' => 2,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 7),
+            ],
+            [
+                'id_transaksi_peminjaman' => 1, // Masih transaksi yang sama
+                'id_alat' => 3,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 10),
+            ],
+            [
+                'id_transaksi_peminjaman' => 2,
+                'id_alat' => 2,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 10),
+            ],
+            [
+                'id_transaksi_peminjaman' => 2,
+                'id_alat' => 10,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 10),
+            ],
+            [
+                'id_transaksi_peminjaman' => 3,
+                'id_alat' => 3,
+                'tanggal_pinjam' => Carbon::create(2024, 11, 1),
+                'tanggal_kembali' => Carbon::create(2024, 11, 10),
+            ]
+        ];
+        DB::table('detail_peminjaman_alat')->insert($detail_alat);
     }
 }
