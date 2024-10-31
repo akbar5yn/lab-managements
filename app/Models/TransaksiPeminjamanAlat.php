f62ecb9fe50ce9ddddccc66349d9ddf9ef175d37
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 01:50:06 +0700

    create model and relation for transaksi-peminjaman-alat

--- app/Models/TransaksiPeminjamanAlat.php
+++ app/Models/TransaksiPeminjamanAlat.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TransaksiPeminjamanAlat extends Model
 {
+    protected $table = 'transaksi_peminjaman_alat';
+    protected $fillable = ['id_user', 'keperluan', 'tanggal_pinjam', 'tanggal_kembali'];
     //
+
+    public function relasiUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function relasiDetailPeminjaman(): HasMany
+    {
+        return $this->hasMany(DetailPeminjamanAlat::class, 'id_alat');
+    }
 }
