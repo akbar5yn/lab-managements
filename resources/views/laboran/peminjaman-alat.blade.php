3cb7d92ee31795fa50767d48c2d910aa9fcd91fd
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 19:43:25 +0700

    make changes to the appearance of data in the tool lending display

--- resources/views/laboran/peminjaman-alat.blade.php
+++ resources/views/laboran/peminjaman-alat.blade.php
@@ -14,39 +14,33 @@
         <section class="h-full overflow-y-scroll rounded-xl bg-white shadow-md">
             <div class="p-4">
                 <div
-                    class="sticky top-0 z-10 grid grid-cols-[4%_20%_25%_25%_15%_auto] items-center border-b border-gray-400 bg-[#e4e4e4] shadow">
+                    class="sticky top-0 z-10 grid grid-cols-[4%_30%_25%_auto_15%] items-center border-b border-gray-400 bg-[#e4e4e4] shadow">
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
                         No</p>
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
-                        Tanggal Kembali</p>
-                    <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
                         Nama Peminjam</p>
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
-                        Barang Dipinjam</p>
+                        No Handphone</p>
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
-                        Status</p>
+                        Total Barang Pinjam</p>
+
                     <p class="flex h-full items-center justify-center px-2 py-2 text-center">
                         Aksi</p>
                 </div>
-                {{-- @foreach ($borrows as $borrow)
-                    <div class="grid grid-cols-[4%_20%_25%_25%_15%_auto] border-b border-gray-400">
+                @foreach ($transaksiPeminjaman as $transaction)
+                    <div class="grid grid-cols-[4%_30%_25%_auto_15%] border-b border-gray-400">
                         <p class="border-r border-gray-400 px-2 py-2 text-center">{{ $loop->iteration }}</p>
-                        <p class="border-r border-gray-400 px-2 py-2">{{ $borrow['tanggal_kembali'] }}</p>
-                        <p class="border-r border-gray-400 px-2 py-2">{{ $borrow['nama_peminjam'] }}</p>
-                        <p class="border-r border-gray-400 px-2 py-2 text-center">{{ $borrow['barang_dipinjam'] }}
-                        <p class="border-r border-gray-400 px-2 py-2 text-center">
-                            <span
-                                class="{{ $borrow['status_peminjaman'] == 'Dipinjam' ? 'bg-yellow-100 text-yellow-600' : ($borrow['status_peminjaman'] == 'Dikembalikan' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600') }} rounded px-2 py-1">
-
-                                {{ $borrow['status_peminjaman'] }}
-                            </span>
-                        </p>
+                        <p class="border-r border-gray-400 px-2 py-2">
+                            {{ $transaction->relasiUser->name ?? 'User tidak ditemukan' }}</p>
+                        <p class="border-r border-gray-400 px-2 py-2">Tambahkan No Hp</p>
+                        <p class="border-r border-gray-400 px-2 py-2">
+                            {{ $transaction->relasi_detail_peminjaman_count }}</p>
                         <div class="flex items-center justify-center gap-5">
-                            <a href="/peminjaman-alat/berlangsung/{{ $borrow['id_request'] }}"
+                            <a href="{{ route('peminjaman.alat.berlangsung.detail', ['id' => $transaction->id]) }}"
                                 class="rounded bg-blue-400 px-2 text-white">Detail</a>
                         </div>
                     </div>
-                @endforeach --}}
+                @endforeach
             </div>
         </section>
     </main>
