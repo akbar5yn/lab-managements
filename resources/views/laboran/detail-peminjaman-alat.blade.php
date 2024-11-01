af2a8675cf888de28b416b6d20068dbabb2fec00
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 19:41:20 +0700

    create new view for handle display detail peminjaman alat

---
+++ resources/views/laboran/detail-peminjaman-alat.blade.php
@@ -0,0 +1,60 @@
+<x-layout>
+    <x-slot:title>
+        <a href="{{ route('peminjaman.alat.berlangsung') }}">
+            {{ $title }}
+        </a>
+    </x-slot:title>
+    <x-slot:role>{{ $role }}</x-slot:role>
+    <x-slot:name>{{ $name }}</x-slot:name>
+
+    <x-slot:subtitle>{{ $subtitle }}</x-slot:subtitle>
+    <!--TODO Tambahkan halaman detail peminjaman untuk tiap peminjam nya agar menampilkan data (tanggal pinjam/kembali, Nama Peminjam, Barang dipinjam, no hp, email, keperluan, stauts peminjaman)-->
+
+    <main class="relative h-full rounded-xl bg-white p-4 shadow-md">
+        <h1 class="mb-5 text-lg font-medium">Informasi Peminjam</h1>
+        <div
+            class="sticky top-0 z-10 grid grid-cols-[25%_25%_25%_auto] items-center border-b border-gray-400 bg-[#e4e4e4] shadow">
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                Nama Peminjam</p>
+
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                Keperluan Peminjaman</p>
+
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                No Handphone</p>
+            <p class="flex h-full items-center justify-center px-2 py-2 text-center">
+                Email</p>
+        </div>
+        <div class="grid grid-cols-[25%_25%_25%_auto] border-b border-gray-400">
+            <p class="border-x border-gray-400 px-2 py-2">{{ $transaksi->relasiUser->name }}</p>
+            <p class="border-r border-gray-400 px-2 py-2">{{ $transaksi->keperluan }}</p>
+            <p class="border-r border-gray-400 px-2 py-2">Belum ada no hp</p>
+            <p class="border-r border-gray-400 px-2 py-2">Belum ada email</p>
+        </div>
+
+        <h1 class="mb-5 mt-10 text-lg font-medium">Informasi Alat</h1>
+
+        <div
+            class="sticky top-0 z-10 grid grid-cols-[25%_25%_25%_auto] items-center border-b border-gray-400 bg-[#e4e4e4] shadow">
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                Barang Dipinjam</p>
+
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                Tanggal Pinjam</p>
+
+            <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
+                Tanggal Kembali</p>
+            <p class="flex h-full items-center justify-center px-2 py-2 text-center">
+                Status</p>
+        </div>
+        @foreach ($transaksi->relasiDetailPeminjaman as $detail)
+            <div class="grid grid-cols-[25%_25%_25%_auto] border-b border-gray-400">
+                <p class="border-x border-gray-400 px-2 py-2">
+                    {{ $detail->relasiUnitBarang->unit->nama_alat }}</p>
+                <p class="border-r border-gray-400 px-2 py-2">{{ $detail->tanggal_pinjam }}</p>
+                <p class="border-r border-gray-400 px-2 py-2">{{ $detail->tanggal_kembali }}</p>
+                <p class="border-r border-gray-400 px-2 py-2">Belum ada status</p>
+            </div>
+        @endforeach
+    </main>
+</x-layout>
