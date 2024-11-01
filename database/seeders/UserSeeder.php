c6132f26978ab2ff3001974ad51c950486acf293
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 20:45:36 +0700

    add new data seeder for user like 'phone_number' and 'email'

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,8 @@ class UserSeeder extends Seeder
                 'nim' => '2000016129',
                 'username' => 'akbar',
                 'password' =>  Hash::make('2000016129'),
+                'email' => 'akbar@gmail.com',
+                'phone_number' => '085266690013',
                 'role' => 'laboran'
             ],
             [
@@ -27,6 +29,8 @@ class UserSeeder extends Seeder
                 'nim' => '2000016130',
                 'username' => 'mifta',
                 'password' =>  Hash::make('2000016130'),
+                'email' => 'mifta@gmail.com',
+                'phone_number' => '085200110022',
                 'role' => 'mahasiswa'
             ],
             [
@@ -34,6 +38,8 @@ class UserSeeder extends Seeder
                 'nim' => '2000016001',
                 'username' => 'farid',
                 'password' =>  Hash::make('2000016001'),
+                'email' => 'farid@gmail.com',
+                'phone_number' => '085200110022',
                 'role' => 'mahasiswa'
             ],
             [
@@ -41,6 +47,8 @@ class UserSeeder extends Seeder
                 'nim' => '2000016003',
                 'username' => 'mifta',
                 'password' =>  Hash::make('2000016003'),
+                'email' => 'teguh@gmail.com',
+                'phone_number' => '085200110022',
                 'role' => 'mahasiswa'
             ]
         ];
