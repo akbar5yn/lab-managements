2e8428fafd1804a891a3b61cb2b0ac2852a107d5
Akbar <akbar5yn.developer@gmail.com>
2024-11-01 19:38:39 +0700

    Enhance PeminjamanAlatController with user, title, and role data binding and relational data for ongoing loan transactions

--- app/Http/Controllers/PeminjamanAlatController.php
+++ app/Http/Controllers/PeminjamanAlatController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransaksiPeminjamanAlat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class PeminjamanAlatController extends Controller
 {
+
+    // SECTION pengajuan peminjaman
     public function pengajuanPeminjaman()
     {
         $user = Auth::user();
@@ -14,15 +17,34 @@ class PeminjamanAlatController extends Controller
         $name = $user->name;
         $role = $user->role;
 
-        return view('laboran.peminjaman-alat', compact('title', 'name', 'role',));
+        return view('laboran.pengajuan-peminjaman-alat', compact('title', 'name', 'role',));
     }
+
+    // SECTION peminjaman berlangsung
 
     public function peminjamanBerlangsung()
     {
         $user = Auth::user();
-        $title = 'Peminjaman Alat & Barang';
+        $title = 'Peminjaman Alat & Barang Berlansung';
         $name = $user->name;
         $role = $user->role;
-        return view('laboran.pengajuan-peminjaman-alat', compact('title', 'name', 'role',));
+        $transaksiPeminjaman = TransaksiPeminjamanAlat::with(['relasiUser', 'relasiDetailPeminjaman'])->withCount('relasiDetailPeminjaman')->get();
+
+        return view('laboran.peminjaman-alat', compact('title', 'name', 'role', 'transaksiPeminjaman'));
+    }
+
+    public function detailPeminjamanBerlangsung($id)
+    {
+        $user = Auth::user();
+        $title = 'Peminjaman Alat & Barang';
+        $subtitle = "Berlangsung";
+        $name = $user->name;
+        $role = $user->role;
+
+        $transaksi = TransaksiPeminjamanAlat::where('id', $id)
+            ->with(['relasiUser', 'relasiDetailPeminjaman'])
+            ->firstOrFail();
+
+        return view('laboran.detail-peminjaman-alat', compact('title', 'subtitle', 'role', 'name', 'transaksi'));
     }
 }
