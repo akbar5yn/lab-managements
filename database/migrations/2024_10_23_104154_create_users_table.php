850aabd3d4db299d75d2c6eed0106cd3dd233fd8
Akbar <akbar5yn.developer@gmail.com>
2024-11-18 02:53:05 +0700

    change type nim to nullable

--- database/migrations/2024_10_23_104154_create_users_table.php
+++ database/migrations/2024_10_23_104154_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('nim');
+            $table->string('nim')->nullable();
             $table->string('prodi')->nullable();
             $table->string('password');
             $table->string('email');
