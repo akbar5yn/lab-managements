a74fb8a3c09f4be13b3129c40be0fe14affb8489
Akbar <akbar5yn.developer@gmail.com>
2024-11-17 21:51:57 +0700

    update label and id from username to nim for login user

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -47,10 +47,10 @@
                     <form class="space-y-6" action="{{ route('authenticate') }}" method="POST">
                         @csrf
                         <div>
-                            <label for="username"
+                            <label for="nim"
                                 class="block text-xs font-medium leading-6 text-[#f6fafd]">Username</label>
                             <div class="relative mt-2">
-                                <input id="username" name="username" type="text"
+                                <input id="nim" name="nim" type="text"
                                     class="block w-full rounded-md border-0 text-xs text-[#265166] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#ffbd97] sm:py-1.5 sm:text-sm sm:leading-6"
                                     required>
                             </div>
