011d81bb4a5baae73e2882127d7d5e70b7ae70ff
Akbar <akbar5yn.developer@gmail.com>
2024-11-27 15:16:43 +0700

    added new some a variable construct, checking date validation, and create function pinjamAlat

--- app/Http/Controllers/PeminjamanAlatController.php
+++ app/Http/Controllers/PeminjamanAlatController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DetailPeminjamanAlat;
 use App\Models\InventarisAlat;
 use App\Models\TransaksiPeminjamanAlat;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class PeminjamanAlatController extends Controller
 {
@@ -13,6 +16,9 @@ class PeminjamanAlatController extends Controller
     protected $name;
     protected $title;
     protected $role;
+    protected $currentTime;
+    protected $startOfDay;
+    protected $endOfDay;
 
     public function __construct()
     {
@@ -21,6 +27,11 @@ class PeminjamanAlatController extends Controller
         $this->name = $user->name;
         $this->title = 'Peminjaman Alat & Barang';
         $this->role = $user->role;
+
+        // ANCHOR set rule hours
+        $this->currentTime = Carbon::now('Asia/Jakarta');
+        $this->startOfDay = $this->currentTime->copy()->setTime(9, 0, 0);
+        $this->endOfDay = $this->currentTime->copy()->setTime(15, 0, 0);
     }
 
     // SECTION pengajuan peminjaman
@@ -95,6 +106,17 @@ class PeminjamanAlatController extends Controller
     // ANCHOR Index page peminjaman alat [Mahasiswa]
     public function informasiAlat()
     {
+        if ($this->currentTime->lessThan($this->startOfDay)) {
+            $minDate = $this->startOfDay->toDateString();
+        } elseif ($this->currentTime->greaterThanOrEqualTo($this->endOfDay)) {
+            $minDate = $this->currentTime->addDay()->setTime(9, 0, 0)->toDateString();
+        } else {
+            $minDate = $this->currentTime->toDateString();
+        }
+
+        $maxDate = $this->currentTime->addDays(5)->toDateString();
+        $minReturnDate = $this->currentTime->copy()->addDay()->toDateString();
+
         $getUnit = InventarisAlat::withCount([
             'alat' => function ($query) {
                 $query->where('kondisi', 'Normal')
@@ -110,12 +132,59 @@ class PeminjamanAlatController extends Controller
             'title' => $this->title,
             'role' => $this->role,
             'user_id' => $this->user_id,
-            'getUnit' => $getUnit
+            'getUnit' => $getUnit,
+            'minDate' => $minDate,
+            'maxDate' => $maxDate,
+            'minReturnDate' => $minReturnDate
         ]);
     }
 
-    public function pinjamAlat()
+    public function pinjamAlat(Request $request)
     {
+        // Validasi data transaksi
+        $validatedTransaksi = $request->validate([
+            'id_user' => 'required',
+            'keperluan' => 'required|string|max:255',
+        ]);
+
+        // Validasi data detail transaksi
+        $validatedDetail = $request->validate([
+            'id_unit' => 'required',
+            'tanggal_pinjam' => 'required|date',
+            'tanggal_kembali' => 'required|date',
+        ]);
+
+        Log::info('Request masuk ke metode pinjamAlat', $request->all());
+
+        try {
+            // Cek apakah ada pengajuan lain untuk alat yang sama pada rentang tanggal yang diajukan
+            $overlappingRequests = DetailPeminjamanAlat::where('id_unit', $validatedDetail['id_unit'])
+                ->where(function ($query) use ($validatedDetail) {
+                    $query->where('tanggal_kembali', '>=', $validatedDetail['tanggal_pinjam']) // Tidak di luar di kiri
+                        ->where('tanggal_pinjam', '<=', $validatedDetail['tanggal_kembali']); // Tidak di luar di kanan
+                })
+                ->where('status', '!=', 'dikembalikan') // Abaikan pengajuan yang ditolak
+                ->get(['tanggal_pinjam', 'tanggal_kembali']);
+
+            // Jika ada pengajuan lain pada tanggal tersebut
+            if ($overlappingRequests->isNotEmpty()) {
+                $dates = $overlappingRequests->map(function ($item) {
+                    return $item->tanggal_pinjam . ' s/d ' . $item->tanggal_kembali;
+                })->implode(', ');
+
+                return redirect()->route('informasi.alat')
+                    ->with('warning', 'Alat sedang dalam tahap pengajuan oleh mahasiswa lain pada rentang tanggal berikut: ' . $dates);
+            }
+
+            // Buat transaksi baru
+            $transaksi = TransaksiPeminjamanAlat::createNewTransaksi($validatedTransaksi);
+            $detail_transaksi = DetailPeminjamanAlat::createNewDetailTransaksi($validatedDetail, $transaksi->id);
+
+            return redirect()->route('informasi.alat')->with('success', 'Peminjaman Anda berhasil dibuat. Tolong lakukan scan di lab untuk melanjutkan peminjaman pada tanggal peminjaman.');
+        } catch (\Throwable $e) {
+            Log::error('Error saat melakukan transaksi peminjaman alat: ' . $e->getMessage());
+            return redirect()->route('informasi.alat')->with('error', 'Terjadi kesalahan saat melakukan peminjaman.');
+        }
     }
 
     public function aktifitasPeminjaman()
