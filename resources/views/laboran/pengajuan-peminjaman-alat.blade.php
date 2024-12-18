f8c2919fd23354fc83f0af6cb1623e11659533f7
Akbar <akbar5yn.developer@gmail.com>
2024-12-18 15:53:41 +0700

    menambahkan button halaman generate qr

--- resources/views/laboran/pengajuan-peminjaman-alat.blade.php
+++ resources/views/laboran/pengajuan-peminjaman-alat.blade.php
@@ -4,11 +4,17 @@
     <x-slot:role>{{ $role }}</x-slot:role>
     <main class="flex h-full flex-col gap-4">
         <!-- SECTION Button pengajuan / peminjaman-->
-        <section class="flex max-w-full gap-4 rounded-xl bg-white p-4 shadow-md">
-            <a href="{{ route('pengajuan.peminjaman.alat') }}"
-                class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
-            <a href="{{ route('peminjaman.alat.berlangsung') }}"
-                class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Berlangsung</a>
+        <section class="flex max-w-full justify-between rounded-xl bg-white p-4 shadow-md">
+            <div class="flex gap-4">
+                <a href="{{ route('pengajuan.peminjaman.alat') }}"
+                    class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
+                <a href="{{ route('peminjaman.alat.berlangsung') }}"
+                    class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Berlangsung</a>
+            </div>
+            <a href="{{ route('qrcode.page') }}"
+                class="{{ request()->is('laboran/qrcode') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">QR
+                Code</a>
+
         </section>
 
         <section class="h-full overflow-y-scroll rounded-xl bg-white shadow-md">
