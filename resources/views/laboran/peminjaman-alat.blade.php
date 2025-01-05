e097fe011ac3ac3d0acac4a4570c7d391a0dc504
Akbar <rizqhaamifta@gmail.com>
2025-01-05 16:17:32 +0700

    menambahkan button navigasi riwayat dan menghapus beberapa status label yang tidak digunakan

--- resources/views/laboran/peminjaman-alat.blade.php
+++ resources/views/laboran/peminjaman-alat.blade.php
@@ -9,6 +9,8 @@
                 class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
             <a href="{{ route('peminjaman.alat.berlangsung') }}"
                 class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Berlangsung</a>
+            <a href="{{ route('riwayat.peminjaman.alat') }}"
+                class="{{ request()->is('laboran/riwayat-peminjaman-alat') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Riwayat</a>
         </section>
 
         <section class="h-full overflow-y-scroll rounded-xl bg-white shadow-md">
@@ -22,7 +24,7 @@
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
                         Nomor Transaksi</p>
                     <p class="flex h-full items-center justify-center border-r border-gray-400 px-2 py-2 text-center">
-                        Status</p>
+                        Status Peminjaman</p>
                     <p class="flex h-full items-center justify-center px-2 py-2 text-center">
                         Aksi</p>
                 </div>
@@ -34,12 +36,10 @@
                         <p class="border-r border-gray-400 px-2 py-2">{{ $transaction->no_transaksi }}</p>
                         <div class="border-r border-gray-400 px-2 py-2 text-center">
                             <p
-                                class="{{ $transaction->status == 'pending' ? 'bg-gray-200 text-gray-600' : ($transaction->status == 'dipinjam' ? 'bg-green-100 text-green-600' : ($transaction->status == 'dikembalikan' ? 'bg-yellow-100 text-yellow-600' : 'bg-red-100 text-red-600')) }} rounded px-2 py-1">
+                                class="{{ $transaction->status == 'dipinjam' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600' }} rounded">
                                 @php
                                     $statusLabels = [
-                                        'belum_dikembalikan' => 'Belum Dikembalikan',
                                         'dipinjam' => 'Dipinjam',
-                                        'dikembalikan' => 'Dikembalikan',
                                         'terlambat_dikembalikan' => 'Terlambat Dikembalikan',
                                     ];
                                 @endphp
