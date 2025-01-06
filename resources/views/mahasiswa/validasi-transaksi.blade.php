cbd38c34bbeb1217cca679ffc9025f17290c6001
Akbar <rizqhaamifta@gmail.com>
2025-01-07 04:12:23 +0700

    memperbaiki kesalahan styling

--- resources/views/mahasiswa/validasi-transaksi.blade.php
+++ resources/views/mahasiswa/validasi-transaksi.blade.php
@@ -8,7 +8,7 @@
         <form class="flex h-full w-full flex-col gap-4" action="{{ route('submit-pengajuan-transaksi') }}" method="POST">
             @csrf
             @method('POST')
-            <section class="flex h-full h-full max-w-full flex-col rounded-xl bg-[#FFFFFF] p-4 shadow-md">
+            <section class="flex h-full max-w-full flex-col rounded-xl bg-[#FFFFFF] p-4 shadow-md">
                 <div
                     class="sticky top-0 z-10 mt-4 grid grid-cols-[15%_20%_20%_15%_15%_auto] items-center border-b border-gray-400 bg-[#F6F8FB] text-center shadow">
                     <p class="h-full border-r border-gray-400 px-2 py-2 font-medium">
