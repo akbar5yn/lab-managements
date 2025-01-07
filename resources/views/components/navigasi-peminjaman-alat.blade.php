090ca306be3b572d82587a85385a0b74f5a011d2
Akbar <rizqhaamifta@gmail.com>
2025-01-07 19:01:29 +0700

    menambahkan pengecekan logika untuk navgiasi mhs dan menambahkan navigasi mhs di component

--- resources/views/components/navigasi-peminjaman-alat.blade.php
+++ resources/views/components/navigasi-peminjaman-alat.blade.php
@@ -1,6 +1,21 @@
-<a href="{{ route('pengajuan.peminjaman.alat') }}"
-    class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
-<a href="{{ route('peminjaman.alat.berlangsung') }}"
-    class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Berlangsung</a>
-<a href="{{ route('riwayat.peminjaman.alat') }}"
-    class="{{ request()->is('riwayat-peminjaman-alat') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Riwayat</a>
+@if (Auth::check() && Auth::user()->role == 'laboran')
+    <a href="{{ route('pengajuan.peminjaman.alat') }}"
+        class="{{ request()->is('laboran/peminjaman-alat/pengajuan') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Pengajuan</a>
+    <a href="{{ route('peminjaman.alat.berlangsung') }}"
+        class="{{ request()->is('laboran/peminjaman-alat/berlangsung') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Berlangsung</a>
+    <a href="{{ route('riwayat.peminjaman.alat') }}"
+        class="{{ request()->is('riwayat-peminjaman-alat') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-4 py-2">Riwayat</a>
+@endif
+
+@if (Auth::check() && Auth::user()->role == 'mahasiswa')
+    <a href="{{ route('informasi.alat') }}"
+        class="{{ request()->is('mahasiswa/informasi-alat') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} w-fit rounded-lg px-2 py-1 text-lg font-medium shadow-sm">
+        Informasi Alat
+    </a>
+    <a href="{{ route('aktivitas.peminjaman') }}"
+        class="{{ request()->is('mahasiswa/peminjaman-alat/aktifitas*') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} w-fit rounded-lg border px-2 py-1 text-lg font-medium shadow-sm">
+        Aktivitas Peminjaman
+    </a>
+    <a href="{{ route('riwayat.peminjaman.alat') }}"
+        class="{{ request()->is('riwayat-peminjaman-alat') ? 'bg-[#2D3648] text-white' : 'border-[2px] border-[#2D3648] text-[#2D3648]' }} rounded-lg px-2 py-1 text-lg font-medium">Riwayat</a>
+@endif
