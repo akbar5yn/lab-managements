d1e9a9fa880904989010888b05902c531ec548e8
Akbar <akbar5yn.developer@gmail.com>
2025-01-21 06:07:59 +0700

    membuat private function untuk membuat instance guna mengambil function dari controller lain

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,7 +1,5 @@
 <?php
 
-//!NOTE Doing code on branch features login
-
 namespace App\Http\Controllers;
 
 use App\Models\InventarisAlat;
@@ -15,70 +13,75 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
+    protected $user;
+    protected $name;
+    protected $title;
+    protected $role;
+    protected $prodi;
+
     public function __construct()
     {
-        // $this->middleware('auth'); // Memastikan semua metode di controller ini dilindungi
+        $this->middleware(function ($request, $next) {
+            $this->user = Auth::user(); // Simpan data user ke properti
+            return $next($request);
+        });
+
+        $this->title = "Dashboard";
+    }
+
+    private function getPengajuanPeminjaman()
+    {
+        $peminjamanAlatController = new PeminjamanAlatController();
+        $pengajuanPeminjaman = $peminjamanAlatController->pengajuanPeminjaman();
+
+        if ($pengajuanPeminjaman->transaksiPengajuanPeminjaman->isEmpty()) {
+            return null;
+        }
+
+        return $pengajuanPeminjaman->transaksiPengajuanPeminjaman->all();
+    }
+
+    private function getTransaksiPeminjaman()
+    {
+        $peminjamanAlatController = new PeminjamanAlatController();
+        $peminjamanBerlangsung = $peminjamanAlatController->peminjamanBerlangsung();
+
+        if ($peminjamanBerlangsung->transaksiPeminjaman->isEmpty()) {
+            return null;
+        }
+
+        return $peminjamanBerlangsung->transaksiPeminjaman->all();
     }
 
     public function indexLaboran()
     {
-        $user = Auth::user();
-        $title = 'Dasboard';
-        $name = $user->name;
-        $role = $user->role;
+
         $totalUnit = Unit::count();
         $totalMhs = User::where('role', 'mahasiswa')->count();
         $unitRusak = Unit::where('kondisi', 'Rusak')->count();
         $totalPengajuan = TransaksiPeminjamanAlat::where('status', 'pending')->count();
-        $schedules = [
-            [
-                'nim' => '2000016001',
-                'name' => 'Teguh Dwi Cahya Kusuma',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '07:00 - 09:00'
-            ],
-            [
-                'nim' => '2000016003',
-                'name' => 'Ansyafarino Arma Wahyudi',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '12:30 - 14:00'
-            ],
-            [
-                'nim' => '2000016004',
-                'name' => 'Daffa',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '14:10 - 15:30'
-            ],
-            [
-                'nim' => '2000016005',
-                'name' => 'Rifky Ramadhan',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '16:00 - 17:00'
-            ],
-            [
-                'nim' => '2000016006',
-                'name' => 'Rifky Ramadhan',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '17:20 - 18:10'
-            ],
-            [
-                'nim' => '2000016002',
-                'name' => 'Alfarizi Dwi Putra',
-                'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
-                'waktu' => '09:20 - 10:00'
-            ],
-        ];
+        $totalPeminjaman = TransaksiPeminjamanAlat::where('status', 'dipinjam')->count();
+
+        $transaksiPengajuanPeminjaman = $this->getPengajuanPeminjaman();
+        $transaksiPeminjaman = $this->getTransaksiPeminjaman();
+
         // Konten dashboard
-        return view('laboran.dashboard', compact('title', 'name', 'role', 'schedules', 'totalUnit', 'totalMhs', 'unitRusak', 'totalPengajuan'));
+        return view('laboran.dashboard', [
+            'title' => $this->title,
+            'name' => $this->user->name,
+            'role' => $this->user->role,
+            'totalUnit' => $totalUnit,
+            'totalMhs' => $totalMhs,
+            'unitRusak' => $unitRusak,
+            'totalPengajuan' => $totalPengajuan,
+            'totalPeminjaman' => $totalPeminjaman,
+            'transaksiPengajuanPeminjaman' => $transaksiPengajuanPeminjaman,
+            'transaksiPeminjaman' => $transaksiPeminjaman
+        ]);
     }
+
     public function indexMahasiswa()
     {
-        $user = Auth::user();
-        $title = 'Dasboard';
-        $name = $user->name;
-        $role = $user->role;
-        $prodi = $user->prodi;
-
         $getUnit = InventarisAlat::withCount([
             'alat' => function ($query) {
                 $query->where('kondisi', 'Normal');
@@ -87,6 +90,13 @@ class DashboardController extends Controller
 
 
         $ruanganTersedia = Ruangan::all();
-        return view('mahasiswa.dashboard', compact('title', 'name', 'role', 'prodi', 'getUnit', 'ruanganTersedia'));
+        return view('mahasiswa.dashboard', [
+            'title' => $this->title,
+            'name' => $this->user->name,
+            'role' => $this->user->role,
+            'prodi' => $this->user->prodi,
+            'getUnit' => $getUnit,
+            'ruanganTersedia' => $ruanganTersedia
+        ]);
     }
 }
