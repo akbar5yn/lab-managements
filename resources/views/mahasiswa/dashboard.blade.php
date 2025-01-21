b25c83cd7acb28479d1d61044dcff34590f90261
Akbar <akbar5yn.developer@gmail.com>
2025-01-21 20:23:08 +0700

    penyesuaian desain dan membuat desain baru untuk menampilkan ketersediaan alat

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -44,10 +44,10 @@
         <section class="flex h-[75%] w-full gap-4">
             <section class="flex h-full w-[50%] max-w-full flex-col rounded-xl bg-[#FFFFFF] p-4 shadow-md">
                 <div class="flex items-center justify-between py-[2px]">
-                    <h2 class="text-lg font-medium">Ketersediaan Alat & Barang</h2>
+                    <h2 class="text-lg font-medium">Jumlah Alat & Barang</h2>
                 </div>
                 <div
-                    class="sticky top-0 z-10 mt-4 grid grid-cols-[40.3%_35.3%_auto] items-center gap-2 border-b border-gray-400 bg-[#F6F8FB] shadow">
+                    class="sticky top-0 z-10 mt-4 grid grid-cols-[40.3%_35.3%_auto] items-center gap-2 rounded-md bg-[#2D3648] text-white shadow">
                     <p class="h-full border-gray-400 px-2 py-2 text-center font-medium">
                         Nama Alat</p>
 
@@ -55,87 +55,61 @@
                         Lokasi</p>
 
                     <p class="h-full border-gray-400 px-2 py-2 text-center font-medium">
-                        Ketersediaan</p>
+                        Jumlah</p>
                 </div>
-                @foreach ($getUnit as $alat)
-                    <div class="grid grid-cols-[40.3%_35.3%_auto] gap-2 rounded-md border-b border-gray-400">
-                        <p class="px-2 py-2">{{ $alat->nama_alat }}</p>
-                        <p class="border-x border-gray-400 px-2 py-2">{{ $alat->lokasi }}</p>
-                        <p class="px-2 py-2 text-center">{{ $alat->alat_count }} Unit</p>
-                    </div>
-                @endforeach
+                <div class="mt-3 flex flex-col gap-2 overflow-y-scroll">
+                    @foreach ($getUnit as $alat)
+                        <div class="grid grid-cols-[40.3%_35.3%_auto] gap-2 rounded-md border border-gray-400 shadow">
+                            <p class="px-2 py-2">{{ $alat->nama_alat }}</p>
+                            <p class="border-x border-gray-400 px-2 py-2">{{ $alat->lokasi }}</p>
+                            <p class="px-2 py-2 text-center">{{ $alat->alat_count }} Unit</p>
+                        </div>
+                    @endforeach
+
+                </div>
+
             </section>
-            <section class="flex h-full w-[50%] max-w-full flex-col gap-4 rounded-xl bg-[#FFFFFF] p-4 shadow-md">
+            <section class="flex h-full w-[50%] max-w-full flex-col rounded-xl bg-[#FFFFFF] p-4 shadow-md">
                 <div class="flex items-center justify-between">
-                    <h2 class="text-lg font-medium">Ketersediaan Ruangan</h2>
-                    <form method="POST" class="m-0 self-center">
-                        @csrf
+                    <h2 class="text-lg font-medium">Ketersediaan Alat & Barang</h2>
+                    <form id="cekForm" action="" method="GET" class="m-0 self-center">
                         <section class="flex items-center gap-4">
-                            <div>
-                                <div x-data="{ isOpen: false }" class="relative inline-block text-left">
-                                    <div>
-                                        <button type="button" @click="isOpen = !isOpen"
-                                            class="inline-flex w-full justify-center gap-x-1.5 rounded-lg border border-gray-400 bg-white px-3 py-1 text-sm font-semibold shadow-sm hover:bg-gray-50"
-                                            id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                            Lab A
-                                            <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-                                                fill="currentColor" aria-hidden="true">
-                                                <path fill-rule="evenodd"
-                                                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </button>
-                                    </div>
-
-
-                                    <div x-show="isOpen" @click.outside="isOpen = false"
-                                        x-transition:enter="transition ease-out duration-100 transform"
-                                        x-transition:enter-start="opacity-0 scale-95"
-                                        x-transition:enter-end="opacity-100 scale-100"
-                                        x-transition:leave="transition ease-in duration-75 transform"
-                                        x-transition:leave-start="opacity-100 scale-100"
-                                        x-transition:leave-end="opacity-0 scale-95"
-                                        class="absolute left-0 z-50 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                        role="menu" aria-orientation="vertical" aria-labelledby="menu-button"
-                                        tabindex="-1">
-                                        <div class="py-1" role="none">
-                                            <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700"
-                                                role="menuitem" tabindex="-1" id="menu-item-0">Lab A</a>
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700"
-                                                role="menuitem" tabindex="-1" id="menu-item-1">Lab B</a>
-                                            <a href="#" class="block px-4 py-2 text-sm text-gray-700"
-                                                role="menuitem" tabindex="-1" id="menu-item-2">Lab C</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <input type="date" value="{{ date('Y-m-d') }}"
+                            <input name="cek_tanggal" type="date" id="cek_tanggal_input"
+                                value="{{ request()->input('cek_tanggal', now()->toDateString()) }}"
                                 class="flex items-center rounded-lg border border-gray-400 py-1 text-sm hover:bg-gray-50">
                         </section>
                     </form>
                 </div>
                 <div
-                    class="sticky top-0 z-10 grid grid-cols-[35.3%_35.3%_auto] items-center gap-2 border-b border-gray-400 bg-[#F6F8FB] shadow">
-                    <p class="h-full border-gray-400 px-2 py-2 font-medium">
+                    class="sticky top-0 z-10 mt-4 grid grid-cols-[40%_35.3%_auto] items-center gap-2 rounded-md border-b border-gray-400 bg-[#2D3648] text-center text-white shadow">
+                    <p class="h-full border-r border-gray-400 px-2 py-2 font-medium">
                         Nama Alat</p>
 
-                    <p class="h-full border-gray-400 px-2 py-2 font-medium">
+                    <p class="h-full border-r border-gray-400 px-2 py-2 font-medium">
                         Lokasi</p>
 
                     <p class="h-full border-gray-400 px-2 py-2 font-medium">
-                        Kapasitas</p>
+                        Ketersediaan</p>
                 </div>
-                @foreach ($ruanganTersedia as $ruangan)
-                    <div>
-                        <div class="grid grid-cols-[35.3%_35.3%_auto] gap-2 rounded-md border border-gray-400">
-                            <p class="px-2 py-2">{{ $ruangan->nama_ruangan }}</p>
-                            <p class="px-2 py-2">{{ $ruangan->lokasi_ruangan }}</p>
-                            <p class="px-2 py-2">{{ $ruangan->kapasitas }}</p>
+                <div class="mt-3 flex flex-col gap-2 overflow-y-scroll">
+                    @foreach ($unitTersedia as $unit)
+                        <div>
+                            <div class="grid grid-cols-[40%_35.3%_auto] gap-2 rounded-md border border-gray-400">
+                                <p class="px-2 py-2">{{ $unit->nama_alat }}</p>
+                                <p class="px-2 py-2">{{ $unit->lokasi }}</p>
+                                <p class="px-2 py-2">{{ $unit->alat_count }}</p>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+
+                </div>
             </section>
         </section>
     </div>
+
+    <script>
+        document.getElementById('cek_tanggal_input').addEventListener('change', function() {
+            document.getElementById('cekForm').submit(); // Mengirimkan form secara otomatis saat tanggal berubah
+        });
+    </script>
 </x-layout>
