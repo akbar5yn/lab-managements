bf99caf24046020a9e555f33e3d6bcfaee2ba6c5
Akbar <akbar5yn.developer@gmail.com>
2025-01-21 20:25:06 +0700

    menghapus filtering pda tampilan informasi pengajuan dan peminjaman alat

--- resources/views/laboran/dashboard.blade.php
+++ resources/views/laboran/dashboard.blade.php
@@ -67,49 +67,6 @@
                 class="sticky top-0 flex h-full max-w-full flex-col gap-4 overflow-y-scroll rounded-xl bg-[#FFFFFF] p-4 shadow-md">
                 <h2 class="text-lg font-medium">Informasi Pengajuan Alat & Barang</h2>
 
-                <!-- ANCHOR Filtering by lab dan tanggal -->
-                <section class="flex items-center justify-between">
-                    <div>
-                        <div x-data="{ isOpen: false }" class="relative inline-block text-left">
-                            <div>
-                                <button type="button" @click="isOpen = !isOpen"
-                                    class="inline-flex w-full justify-center gap-x-1.5 rounded-lg border bg-white px-3 py-1 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                    id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                    Lab A
-                                    <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </div>
-
-
-                            <div x-show="isOpen" @click.outside="isOpen = false"
-                                x-transition:enter="transition ease-out duration-100 transform"
-                                x-transition:enter-start="opacity-0 scale-95"
-                                x-transition:enter-end="opacity-100 scale-100"
-                                x-transition:leave="transition ease-in duration-75 transform"
-                                x-transition:leave-start="opacity-100 scale-100"
-                                x-transition:leave-end="opacity-0 scale-95"
-                                class="absolute left-0 z-20 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                                <div class="py-1" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-0">Lab A</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-1">Lab B</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-2">Lab C</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </section>
-
                 <!-- ANCHOR Table -->
                 <section class="flex h-full flex-col gap-5 overflow-y-scroll">
                     <div
@@ -148,49 +105,7 @@
                 class="sticky top-0 flex h-full max-w-full flex-col gap-4 overflow-y-scroll rounded-xl bg-[#FFFFFF] p-4 shadow-md">
                 <h2 class="text-lg font-medium">Informasi Penggunaan Alat & Barang</h2>
 
-                <!-- ANCHOR Filtering by lab dan tanggal -->
-                <section class="flex items-center justify-between">
-                    <div>
-                        <div x-data="{ isOpen: false }" class="relative inline-block text-left">
-                            <div>
-                                <button type="button" @click="isOpen = !isOpen"
-                                    class="inline-flex w-full justify-center gap-x-1.5 rounded-lg border bg-white px-3 py-1 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                    id="menu-button" aria-expanded="true" aria-haspopup="true">
-                                    Lab A
-                                    <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                                        aria-hidden="true">
-                                        <path fill-rule="evenodd"
-                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                                            clip-rule="evenodd" />
-                                    </svg>
-                                </button>
-                            </div>
 
-
-                            <div x-show="isOpen" @click.outside="isOpen = false"
-                                x-transition:enter="transition ease-out duration-100 transform"
-                                x-transition:enter-start="opacity-0 scale-95"
-                                x-transition:enter-end="opacity-100 scale-100"
-                                x-transition:leave="transition ease-in duration-75 transform"
-                                x-transition:leave-start="opacity-100 scale-100"
-                                x-transition:leave-end="opacity-0 scale-95"
-                                class="absolute left-0 z-20 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                role="menu" aria-orientation="vertical" aria-labelledby="menu-button"
-                                tabindex="-1">
-                                <div class="py-1" role="none">
-                                    <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-0">Lab A</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-1">Lab B</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
-                                        tabindex="-1" id="menu-item-2">Lab C</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </section>
 
                 <!-- ANCHOR Table -->
                 <section class="flex h-full flex-col gap-5 overflow-y-scroll">
