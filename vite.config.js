d0677e8d341178261c4d3719402c3e1351dd38d1
Akbar <akbar5yn.developer@gmail.com>
2025-02-03 03:16:47 +0700

    install package dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
