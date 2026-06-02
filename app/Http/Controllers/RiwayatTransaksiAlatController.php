867e0b3d8866951228158e3fa10d8af9cf12cadb
akbarsyn <akbar.p.suryamin@gli.id>
2026-06-02 15:15:15 +0700

    import DB

--- app/Http/Controllers/RiwayatTransaksiAlatController.php
+++ app/Http/Controllers/RiwayatTransaksiAlatController.php
@@ -7,6 +7,7 @@ use App\Models\TransaksiPeminjamanAlat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class RiwayatTransaksiAlatController extends Controller
 {
